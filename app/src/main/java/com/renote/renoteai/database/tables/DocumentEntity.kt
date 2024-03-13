4106ed99496ac70ad4338745967a93e08be6d91f
komal-renoteai <komala@renote.ai>
2024-03-13 11:52:17 +0530

    Room Database tables updated

--- app/src/main/java/com/renote/renoteai/database/tables/DocumentEntity.kt
+++ app/src/main/java/com/renote/renoteai/database/tables/DocumentEntity.kt
@@ -6,18 +6,16 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "Document_table")
 data class DocumentEntity(
     @PrimaryKey(autoGenerate = true)
-    val  idd:Int = 0,
     val id: String,
     val name: String,
     val createdDate: Long,
     val updatedDate: Long,
-    val fileData: String,
+    val folderId:String,
     val isSynced: Boolean,
     val isPin: Boolean,
     val isFavourite: Boolean,
-    val folderId:String,
+    val fileData: String,
     val openCount:Int,
-    val localFilePathIos:String,
     val localFilePathAndroid:String,
     val tagId:String,
     val driveType:String,
